e032a905b25b57c17ef01b6403e50231340d2142
Chris Boutiette <cboutiette@shelltechworks.com>
2017-12-06 00:06:56 -0500

    CLEAN the react starter kit to be ready for the lesson

--- javascripts/react-sandbox/src/App.js
+++ javascripts/react-sandbox/src/App.js
@@ -1,34 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-class VideoGamesList extends React.Component {
-    render() {
-        const vidGames = this.props.vidGames
-
-        return <ol> {
-            vidGames.map((game, index) => (
-            <li key={index}>{game.name}</li>
-        ))}
-        </ol>
-    }
-}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-          <VideoGamesList vidGames={[
-              { name: 'Doom'},
-              { name: 'MarioKart 64'},
-              { name: 'Zelda'}
-          ]}/>
-          <VideoGamesList vidGames={[
-              { name: 'Mario Party'},
-              { name: 'CounterStrike'}
-          ]}/>
-      </div>
-    );
   }
 }
 
