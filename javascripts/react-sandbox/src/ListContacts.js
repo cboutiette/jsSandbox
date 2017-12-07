7c359611c8c2b1189cc448ecf0cc59a8486f84eb
Chris Boutiette <cboutiette@shelltechworks.com>
2017-12-06 22:28:17 -0500

    ADD stateless function method to ListContacts.js file

--- javascripts/react-sandbox/src/ListContacts.js
+++ javascripts/react-sandbox/src/ListContacts.js
@@ -1,6 +1,35 @@
 import React, {Component} from 'react'
 
-class ListContacts extends Component
+/**
+ * As a stateless function you can do this:
+ */
+function ListContacts (props)
+{
+    return (
+        <ol className='contact-list' >
+            {props.contacts.map((contact) => (
+                <li key={contact.id} className='contact-list-item'>
+                    <div className='contact-avatar' style={{
+                        backgroundImage: `url(${contact.avatarURL})`
+                    }}/>
+                    <div className='contact-details'>
+                        <p>{contact.name}</p>
+                        <p>{contact.email}</p>
+                    </div>
+                    <button className='contact-remove'>
+                        Remove
+                    </button>
+                </li>
+            ))}
+        </ol>
+    )
+}
+
+
+/**
+ * As a class component you can do the following:
+ */
+class ListContactsClassComponent extends Component
 {
     render() {
         console.log('Props', this.props)
@@ -26,4 +55,3 @@ class ListContacts extends Component
 }
 
 export default ListContacts
-
