ad3b9a5554a0f660aad3923e0d0e45a5b72b13e8
Chris Boutiette <cboutiette@shelltechworks.com>
2017-12-12 22:36:55 -0500

    CLEAN the code that was used for testing and added an input to the list contacts component

--- javascripts/react-sandbox/src/App.js
+++ javascripts/react-sandbox/src/App.js
@@ -6,7 +6,6 @@ import ListContacts from './ListContacts'
 
 class App extends Component {
     state = {
-        searchbox: "",
         contacts: [
             {
                 "id": "ryan",
@@ -29,36 +28,18 @@ class App extends Component {
         ]
     }
 
-    // If you notice I put the log message in the callback to setState
-    // This guarantees that te
-    handleChange = (event) => {
-        this.setState({searchbox: event.target.value},
-            () => {console.log('The searchbox now contains:' + this.state.searchbox)});
-    }
-
     removeContact = (contact) => {
         this.setState((prevState) => ({
             contacts: prevState.contacts.filter(c => c.id !== contact.id)
         }))
     }
 
-    /*
-    // Just testing if I can make it only render after clicking the delete button when
-    // the searchbox has the secret "delete" text.
-    shouldComponentUpdate(){
-        if(this.state.searchbox === "delete")
-            return true
-        return false
-    }
-    */
-
     // This is an example of using an ES6 function with an implicit return from ListContact.js
     // <ListContacts2 contacts={contacts}/>
     render() {
     return(
         <div>
-            <input onChange={this.handleChange}></input>
-            <ListContacts OnDeleteContact={this.removeContact} contacts={this.state.contacts}/>
+            <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
         </div>
     )
   }
--- javascripts/react-sandbox/src/ListContacts.js
+++ javascripts/react-sandbox/src/ListContacts.js
@@ -1,30 +1,56 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 /**
  * Precondition: You only require a render method
  * As a stateless function you can do this:
  */
-function ListContacts (props)
-{
-    return (
-        <ol className='contact-list' >
-            {props.contacts.map((contact) => (
-                <li key={contact.id} className='contact-list-item'>
-                    <div className='contact-avatar' style={{
-                        backgroundImage: `url(${contact.avatarURL})`
-                    }}/>
-                    <div className='contact-details'>
-                        <p>{contact.name}</p>
-                        <p>{contact.email}</p>
-                    </div>
-                    <button onClick={() => props.OnDeleteContact(contact)} className='contact-remove'>
-                        Remove
-                    </button>
-                </li>
-            ))}
-        </ol>
-    )
+class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState({query: query.trim()})
+    }
+
+    render() {
+        return (
+            <div className='list-contacts'>
+                {JSON.stringify(this.state)}
+                <div className='list-contacts-top'>
+                    <input
+                        className='search-contacts'
+                        type='text'
+                        placeholder='Search Contacts'
+                        value={this.state.query}
+                        onChange={(event) => this.updateQuery(event.target.value)}
+                    />
+                </div>
+                <ol className='contact-list'>
+                    {this.props.contacts.map((contact) => (
+                        <li key={contact.id} className='contact-list-item'>
+                            <div className='contact-avatar' style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}/>
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+                                Remove
+                            </button>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
 }
 
 /**
@@ -81,9 +107,6 @@ class ListContactsClassComponent extends Component
 }
 */
 
-ListContacts.propTypes = {
-    contacts: PropTypes.array.isRequired,
-    OnDeleteContact: PropTypes.func.isRequired
-}
+
 
 export default ListContacts
