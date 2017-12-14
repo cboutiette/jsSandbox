7c0c5ec3fba0b0eb38997e976faf10f3d080b92d
Chris Boutiette <cboutiette@shelltechworks.com>
2017-12-13 22:33:31 -0500

    ADD calls to the service using out contacts api file

--- javascripts/react-sandbox/src/App.js
+++ javascripts/react-sandbox/src/App.js
@@ -1,37 +1,26 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 // This is used as an example of how to import a property:
 // import ListContacts2 from './ListContacts'
 
 class App extends Component {
     state = {
-        contacts: [
-            {
-                "id": "ryan",
-                "name": "Ryan Florence",
-                "email": "ryan@reacttraining.com",
-                "avatarURL": "http://localhost:5001/ryan.jpg"
-            },
-            {
-                "id": "michael",
-                "name": "Michael Jackson",
-                "email": "michael@reacttraining.com",
-                "avatarURL": "http://localhost:5001/michael.jpg"
-            },
-            {
-                "id": "tyler",
-                "name": "Tyler McGinnis",
-                "email": "tyler@reacttraining.com",
-                "avatarURL": "http://localhost:5001/tyler.jpg"
-            }
-        ]
+        contacts: []
+    }
+
+    componentDidMount(){
+        ContactsAPI.getAll().then((contacts) => {
+            this.setState({contacts})
+        })
     }
 
     removeContact = (contact) => {
         this.setState((prevState) => ({
             contacts: prevState.contacts.filter(c => c.id !== contact.id)
         }))
+        ContactsAPI.remove(contact)
     }
 
     // This is an example of using an ES6 function with an implicit return from ListContact.js
