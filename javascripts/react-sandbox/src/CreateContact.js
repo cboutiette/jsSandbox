27cf4e657ff12075231e70b610bcba51187d49b8
Chris Boutiette <cboutiette@shelltechworks.com>
2017-12-21 23:29:15 -0500

    ADD form and ImageInput to createcontact component

--- javascripts/react-sandbox/src/CreateContact.js
+++ javascripts/react-sandbox/src/CreateContact.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import ImageInput from './ImageInput'
 
 class CreateContact extends Component
 {
     render(){
-        return (<div>Create Component!</div>)
+        return (
+            <div>
+                <Link className="close-create-contact" to="/" >Close</Link>
+                <form className="create-contact-form">
+                    <ImageInput className="create-contact-avatar-input" maxHeight={64} name="avatarURL" />
+                    <div className="create-contact-details">
+                        <input type="text" name="name" placeholder="Name"/>
+                        <input type="text" name="email" placeholder="Email"/>
+                        <button>Submit</button>
+                    </div>
+                </form>
+            </div>)
     }
 }
 
